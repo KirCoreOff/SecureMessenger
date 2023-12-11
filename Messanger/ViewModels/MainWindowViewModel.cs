46fcf4e304b315c15c1c05dd4492c9efadbf3230
Miraj <mirajiik@yandex.ru>
2023-12-11 22:01:17 +0500

    Added Views

---
+++ Messanger/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Messanger.Infrastructure.Commands;
+using Messanger.ViewModels.Base;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Messanger.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Commands
+        #region CloseApplicationCommand
+        public ICommand CloseApplicationCommmand { get; }
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        #endregion
+        #endregion
+
+        #region Название окна
+        private string _TitleWindow = "Hello World!";
+        public string TitleWindow
+        {
+            get => _TitleWindow;
+            set => Set(ref _TitleWindow, value);
+        } 
+        #endregion
+            
+        public MainWindowViewModel()
+        {
+            #region Commands
+            CloseApplicationCommmand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            #endregion
+        }
+    }
+}
